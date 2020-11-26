725a75d1e6bc91dcf3adf7d31530037fa2a5d4e8
Bruce Farmer <71850014+bcefarmer@users.noreply.github.com>
2020-11-25 22:23:05 -0500

    date-time logic

--- js/events.js
+++ js/events.js
@@ -17,21 +17,81 @@ THEN the saved events persist
 
 $( document ).ready(function(){
 
-
+//------------------------------
+   /*
+   MOMENT.JS
+   Purpose: Easy date conversion.
+   Used for date displays and defining local storage objects.
+   */  
+  //------------------------------
+       // Current Date
     var now = moment();
     var nowFormat = now.format("dddd, MMMM Do");
     $("#currentDay").text(nowFormat);
 
-/*    
+       // Current Timestamp
+    var time = moment();
+    var timeFormat = time.valueOf();
+
+ /*      
 Because this app uses a modal box to collect information, variable global_current_record
 is used to hold the id of the time-slot that was originally clicked.
 */ 
 
-let global_current_record = "";
+let global_current_record = ""; 
 
-/*    
-On clicking time-block, bring up a modal to enter event information.
-*/ 
+
+// Check to see if a local storage object exists for the current date.  If so
+// run function to populate previously-created events from storage.
+
+if(localStorage.getItem(nowFormat) != undefined && localStorage.getItem(nowFormat) != null && localStorage.getItem(nowFormat) != "" ){
+   import_localStorage_events();
+ }
+   
+ 
+//------------------------------
+   /*
+   import_localStorage_events()
+   PURPOSE: Bringing in events created for a certain day that are held
+            in the device's local storage.
+
+   PARAMETERS: N/A
+
+   RETURNS: N/A
+   */  
+  //------------------------------
+
+
+function import_localStorage_events(){
+
+
+  var grabLocal = localStorage.getItem(nowFormat);
+  var eventMap = JSON.parse(grabLocal);
+  
+  
+  for (var i = 0; i<eventMap.length; i++){
+    workingObj = eventMap[i];
+   
+    workingId ="#" + workingObj.id;
+    workingText=workingObj.eventText;
+    var dataAssignment_label = "[data-assignment=" + "\"" + workingObj.id + "\"" + "]"
+    $(dataAssignment_label).text(workingText);
+  
+  }
+}
+  
+//------------------------------
+   /*
+   Modal window on-click function
+   PURPOSE: When user clicks on a time-block, bring up a modal 
+   to enter event information.  Populates variable global_current_record.
+
+   PARAMETERS: N/A
+
+   RETURNS: N/A
+   */  
+  //------------------------------
+ 
 
 $(".time-block").on("click", function(event){
 event.stopImmediatePropagation();
@@ -40,10 +100,6 @@ targetElement = event.target.closest("section");
 var identifyBlock = targetElement.id;
 
 
-// *******************
-//alert(currentContent);
-// *******************
-
 var identifyTime = targetElement.getAttribute("data-time");
 global_current_record = identifyBlock;
 $("#eventModal").modal('show');
@@ -51,9 +107,20 @@ $(".modal-title").text(identifyTime);
 var test;
 })
 
-/*    
-After user clicks button to submit event......
-*/ 
+//------------------------------
+   /*
+  on-click function to submit event on modal window.
+
+  PURPOSE: When user clicks button on modal to submit an event, this script
+  populates the <p> element on each time block with the event.  Before doing so,
+  checks to make sure the time-block is empty.
+
+
+   PARAMETERS: N/A
+
+   RETURNS: N/A
+   */  
+  //------------------------------
 
 $("#insertEvent").on("click", function(event){
     event.preventDefault();
@@ -71,37 +138,43 @@ $("#insertEvent").on("click", function(event){
         $(idString).find("p").text(trimmedInput);    
     }
 
-
-    
     global_current_record = "";
     $("#eventModal").modal('hide');
     alter_localStorageObject();
     })
 
-/*
-  LOCAL STORAGE
-*/
+//------------------------------
+   /*
+   alter_localStorageObject()
+
+   PURPOSE: Each time a new event is submitted, creates an array filled with
+   JSON objects pertaining to all daily events, and tucks it into local storage
+   for that day.
+
+   PARAMETERS: N/A
+
+   RETURNS: N/A
+   */  
+  //------------------------------
 
 function alter_localStorageObject(){
    let event_array = [];
-  
-       
- $("section").each( function(){
+   $("section").each( function(){
    //var current_section = $(this);
-  
-   
-   
-    if( $(this)[0].children[0].childNodes[3].children[0].innerHTML != null && $(this)[0].children[0].childNodes[3].children[0].innerHTML != undefined && $(this)[0].children[0].childNodes[3].children[0].innerHTML != null  && $(this)[0].children[0].childNodes[3].children[0].innerHTML != "" ){
-                        var currentLine ="{id :" + $(this)[0].id + " , " + "eventText : " + $(this)[0].children[0].childNodes[3].children[0].innerHTML + "}";
+        if( $(this)[0].children[0].childNodes[3].children[0].innerHTML != null && $(this)[0].children[0].childNodes[3].children[0].innerHTML != undefined && $(this)[0].children[0].childNodes[3].children[0].innerHTML != null  && $(this)[0].children[0].childNodes[3].children[0].innerHTML != "" ){
+                        var currentLine = {
+                                            "id" :  $(this)[0].id  , 
+                                            "eventText" : $(this)[0].children[0].childNodes[3].children[0].innerHTML, 
+                                          };
                         event_array.push(currentLine);
                        }
                       
-                      
-    
-    var objString =  JSON.stringify(event_array);
+    var objString = JSON.stringify(event_array);
     localStorage.setItem(nowFormat, objString);
     
 
-                      })
-}
-})                    
+                      })}
+
+
+
+  })                    
