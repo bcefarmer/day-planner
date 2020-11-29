4a9e0cd62a9c33953fb2920eba9a8ce46b8d6b12
Bruce Farmer <71850014+bcefarmer@users.noreply.github.com>
2020-11-29 16:45:10 -0500

    bring in previously defined event info, in case user simply wants to edit.

--- js/events.js
+++ js/events.js
@@ -1,54 +1,43 @@
+
 /*
-GIVEN I am using a daily planner to create a schedule
-WHEN I open the planner
-THEN the current day is displayed at the top of the calendar
-WHEN I scroll down
-THEN I am presented with time blocks for standard business hours
-WHEN I view the time blocks for that day
-THEN each time block is color-coded to indicate whether it is in the past, present, or future
-WHEN I click into a time block
-THEN I can enter an event
-WHEN I click the save button for that time block
-THEN the text for that event is saved in local storage
-WHEN I refresh the page
-THEN the saved events persist
+Perform when DOM safely loads
 */
-
-
 $( document ).ready(function(){
 
 //------------------------------
    /*
    MOMENT.JS
-   Purpose: Easy date conversion.
+   PURPOSE: Easy date/time conversion and comparison.
    Used for date displays and defining local storage objects.
    */  
-  //------------------------------
+//------------------------------
 
        // Create a display date with moment.js that represents current date.
     var now = moment();
     var nowFormat = now.format("dddd, MMMM Do");
 
 
-      // Populate data-date on on all timeslot section elements.  Make sure current
+      // Populate attribute "data-date" on on all section elements.  Make sure current
       // date shows at top of screen as well.
     $("section").attr("data-date", nowFormat);
     $("#currentDay").text(nowFormat);
     
-
+    
+    // After initial run, populatePastPresent() performs on a timer.
+    // More on this function below.
     populatePastPresent();
-    setInterval(function(){ populatePastPresent(); }, 3000);
+    setInterval(function(){ populatePastPresent(); }, 60000);
 
     //------------------------------
    /*
    populatePastPresent();
 
-   Purpose: Compares times to current time, and changes the background
+   PURPOSE: Compares times to current time, and changes the background
    color of each timeblock circle according to past and future.
 
-   Parameters: N/A
+   PARAMETERS: N/A
 
-   Returns: N/A
+   RETURNS: N/A
   
    */  
   //------------------------------ 
@@ -59,12 +48,20 @@ $( document ).ready(function(){
     var nowMilli = moment().unix();
 
     $(".time-block").each(function(){
+      // there are two programmer-defined data attributes in each section element
+      // that are used to judge if the particular .time-block is "current". In more concrete
+      // language, each loop run judges if the current time MIGHT fall between, say,
+      // "data-time" 1/1/2020 8:00 am and "data-nextSlot" 1/1/2020 9:00 am.  This is
+      // how the circles in each time slot are colored to represent wheter the time
+      // block is past, present, or future.
+      
       var dTime = $(this).attr("data-time");
       var next_hour = $(this).attr("data-nextSlot");
       var fullString = activeDate + " " + dTime;
       var next_timeSlot = activeDate + " " + next_hour;
       let e_factor;
-
+      
+      // Compare now to each time slot
       if(nowMilli >= moment(fullString).unix() && nowMilli <= moment(next_timeSlot).unix() ){
         $(this).find("span").css("background-color", "orange");
         $(this).find("span").css("height", "50px");
@@ -79,7 +76,7 @@ $( document ).ready(function(){
         $(this).find("span").css("background-color", "#20B2AA");
   }
   })};   
-     
+     // ****** End of populatePastPresent() function.
 
   
 /*      
@@ -89,9 +86,10 @@ is used to hold the id of the time-slot that was originally clicked.
 
 let global_current_record = ""; 
 
+
 //--------------------------------
 /*
-Check to see if a local storage object exists for the current date.  If so
+PURPOSE: Check to see if a local storage object exists for the current date.  If so
 run function to populate previously-created events from storage.  Basically
 just a function that evaluates and points to another function.
 */
@@ -151,15 +149,30 @@ targetElement = event.target.closest("section");
 //var targetElement = event.target;
 var identifyBlock = targetElement.id;
 
+// find out if <p> element within selected section contains event info
+var loadContent = "[data-assignment=" + "\"" + identifyBlock + "\"" + "]"
+var insideParagraph = $(loadContent).text();
+var stringContent = "";
+
+if($("insideParagraph").is(':empty') || $("insideParagraph") === null || $("insideParagraph") === undefined  ){
+   stringContent = "";
+}
+else{
+  stringContent = insideParagraph; // If that time-slot already contains text, gather it.
+}
+
 // grabs the string of the selected time.  this is stored is data attribute "data-time."
+
 var identifyTime = targetElement.getAttribute("data-time");
 // sets global_current_record to recognize which time block to change when "submit" button
 // is pressed.
 global_current_record = identifyBlock;
 $("#eventModal").modal('show');
 $(".modal-title").text(identifyTime);
-
+$("#enter-data").val("");
+$("#enter-data").val(stringContent);
 })
+
 
 //------------------------------
    /*
@@ -167,7 +180,8 @@ $(".modal-title").text(identifyTime);
 
   PURPOSE: When user clicks button on modal to submit an event, this script
   populates the <p> element on each time block with the event.  Before doing so,
-  checks to make sure the time-block is empty.
+  checks to make sure the time-block is empty.  If it isn't, deletes the content that
+  was there and populates <p> tags.
 
 
    PARAMETERS: N/A
